27d8e8ac01ff1c092e60388edd39165bf2deae46
Gal Zafar <theycallmefachfach@gmail.com>
2023-11-15 00:06:30 +0200

    Add Action enum and choose_action method to ProportionalController.
    NOT WORKING

--- main.py
+++ main.py
@@ -12,11 +12,20 @@ at first, ill take the error as the angle of the pole from the upright position.
 """
 from abc import ABC, abstractmethod
 
+
+
+class Action(enum.Enum):
+    RIGHT = 1,
+    LEFT = 0
+
+
 class ControllerComponent(ABC):
     """
     This is the base class for all controller components
     """     
     pass
+    error = 0.0
+
 
     @abstractmethod
     def step(self):
@@ -24,13 +33,28 @@ class ControllerComponent(ABC):
         This is called at each time step
         """
         pass
-class Action(enum.Enum):
-    RIGHT = 1,
-    LEFT = 0
+
+
+    @abstractmethod
+
+    def choose_action(self, error: float) -> Action:
+        """
+        The error represents the angle of the pole from its upright position 
+        """
+        pass
+
 
 class ProportionalController(ControllerComponent):
+    def step(self):
+
+        pass
     
-    pass
+    def choose_action(self, error: float) -> Action:
+        chosen_action = Action.RIGHT
+
+
+        return chosen_action
+
 
 class Controller:
     
@@ -42,7 +66,7 @@ class Controller:
         """
         takes error as input and returns the action to perform
         """
-        return Action.RIGHT
+        pass
     
     def step(self):
         """
@@ -58,18 +82,18 @@ class Controller:
 
 observation, info = env.reset(seed=42)
 
-for _ in range(1000):    
+for time_step in range(1000):    
     env.render()
     # action = env.action_space.sample()
-    p_controller = ProportionalController()
-    controller = Controller([p_controller])
+    p_component = ProportionalController()
+    controller = Controller([p_component])
     action = controller.choose_action(observation)
     # action = 1
     # action = 0
     observation, reward, terminated, truncated, info = env.step(action)
     controller.step()
     
-    print(f"angle: {observation[2]} | iteration: {_} | action: {action} | info: {info}")
+    print(f"angle: {observation[2]} | iteration: {time_step} | action: {action} | info: {info}")
     # unpack observation
     cart_position, cart_velocity, pole_angle, pole_velocity = observation
     if terminated or truncated:
