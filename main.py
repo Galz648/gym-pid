70538c04d728c3acf61c347dd38ef3637f34ce1a
Gal Zafar <theycallmefachfach@gmail.com>
2023-11-19 14:09:35 +0200

    WIP: Add lunar lander flight controller sketch

--- main.py
+++ main.py
@@ -1,103 +1,64 @@
 import gymnasium as gym
 import time
 import enum
-env = gym.make("CartPole-v1",render_mode='human')
-"""
-I need to add visualization for the cart thingy
-I want this controller to be flexible, where I could attach and remove any of the three components (proportional/integral,derivative)
-at each time step the error needs to be calculated
-
-at first, ill take the error as the angle of the pole from the upright position.
+import gymnasium as gym
+from typing import List
 
 """
-from abc import ABC, abstractmethod
+This is an attempt at creating a flight controller for the lunar lander project.
+This is a continuous action space problem. The action space is a vector of 3 floats.
+The first float is the main thruster power, the second float is the left thruster power, and the third float is the right thruster power.
+lets start with something simple, only thrusting the main engine at some constant speed.
 
+my strategy would be to try and stabilize the moon lander, using a proportional reaction to the angle  of the lander.
+starting with the discrete action would be simpler since I wouldnt have to understand or manage the power at which the thrusters are acting.
+"""
+class ActionType(enum.Enum):
+    MOVE_FORWARD = 1
+    MOVE_BACKWARD = 2
+    TURN_LEFT = 3
+    TURN_RIGHT = 4
 
+    def __init__(self, value):
+        self.value = value
 
-class Action(enum.Enum):
-    RIGHT = 1,
-    LEFT = 0
-
-
-class ControllerComponent(ABC):
-    """
-    This is the base class for all controller components
-    """     
-    pass
-    error = 0.0
-
-
-    @abstractmethod
-    def step(self):
-        """
-        This is called at each time step
-        """
-        pass
-
-
-    @abstractmethod
-
-    def choose_action(self, error: float) -> Action:
-        """
-        The error represents the angle of the pole from its upright position 
-        """
-        pass
-
-
-class ProportionalController(ControllerComponent):
-    def step(self):
-
-        pass
-    
-    def choose_action(self, error: float) -> Action:
-        chosen_action = Action.RIGHT
-
-
-        return chosen_action
-
+    def get_float_argument(self):
+        return self.value
 
 class Controller:
-    
-    def __init__(self, components: list[ControllerComponent]):
-        self.components = components
-
-
-    def choose_action(self, error: float) -> Action:
-        """
-        takes error as input and returns the action to perform
-        """
+    @staticmethod
+    def decide_action(angle: float) -> ActionType:
+        # Given the angle at which the lander is oriented, choose an action that is opposite to this orientation
         pass
-    
-    def step(self):
-        """
-        This is called at each time step
-
-        updates each of the controller components.
-        """
-        for component in self.controller:
-            component.step()
 
 
 
+def main():
+    env = gym.make("LunarLander-v2",render_mode='human',continuous=True)
+    observation, info = env.reset(seed=42)
+    for _ in range(1000): # 
+        action = env.action_space.sample()  # this is where you would insert your policy
+        print(f"action: {action}")
+        observation, _, terminated, truncated, info = env.step(action)
+        # print(f"observation: {observation}")
+        x,y = observation[0], observation[1]
+        # x_linear_velocity, y_linear_velocity = observation[2], observation[3]
+        angle = observation[4]
+        # angular_velocity = observation[5]
+        # left_leg_contact:bool = observation[6]
+        # right_leg_contact: bool = observation[7]
 
-observation, info = env.reset(seed=42)
+        # print all in different lines
+        print(f"action: {action}")
+        print(f"(x,y):({x},{y})")
+        print(f"angle:{angle}")   
+        env.render()
+        time.sleep(0.1)
+        if terminated or truncated:
+            observation, info = env.reset()
+    env.close()
 
-for time_step in range(1000):    
-    env.render()
-    # action = env.action_space.sample()
-    p_component = ProportionalController()
-    controller = Controller([p_component])
-    action = controller.choose_action(observation)
-    # action = 1
-    # action = 0
-    observation, reward, terminated, truncated, info = env.step(action)
-    controller.step()
-    
-    print(f"angle: {observation[2]} | iteration: {time_step} | action: {action} | info: {info}")
-    # unpack observation
-    cart_position, cart_velocity, pole_angle, pole_velocity = observation
-    if terminated or truncated:
-        print("terminated or truncated")
-        observation, info = env.reset()
-    # time.sleep(0.2)
-env.close()
+
+main()
+
+
